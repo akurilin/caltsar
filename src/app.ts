ee5b99bada28667f6f1925ec0c16697a1529ad65
Alex Kurilin <alex@kurilin.net>
2021-10-04 01:25:12 -0700

    Add manual auth routes for calendar events

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
 import http from "http";
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
 import * as dotenv from "dotenv";
+import { google, Auth, calendar_v3 } from "googleapis";
 
 dotenv.config();
 
-if (!process.env.PORT) {
+if (!process.env.PORT || !process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
   process.exit(1);
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "http://localhost:3000/oauth2callback"
+);
 
 const app: Express = express();
 
@@ -45,8 +52,70 @@ app.use((req, res, next) => {
 
 /** Routes */
 app.get("/", (request: Request, response: Response) => {
-  console.log(request.url);
-  response.json({ message: "Welcome to the root URL" });
+  const scopes = ["https://www.googleapis.com/auth/calendar.events.readonly"];
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: "offline",
+
+    // If you only need one scope you can pass it as a string
+    scope: scopes,
+  });
+  response.json({ authUrl: url });
+});
+
+app.get(
+  "/oauth2callback",
+  async (request: Request, response: Response, next: NextFunction) => {
+    // might want to check that you actually did get the code here
+    // console.log(request.query.code);
+
+    if (!request.query.code) {
+      throw new Error("Code missing from Oauth2 Callback");
+    }
+
+    const code: string = request.query.code.toString();
+
+    // console.log(code);
+
+    // This will provide an object with the access_token and refresh_token.
+    // Save these somewhere safe so they can be used at a later time.
+    // const {tokens} = await oauth2Client.getToken(code)
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
+
+    response.json({ message: "Success: Oauth2 client credentials set!" });
+  }
+);
+
+app.get("/events", async (request: Request, response: Response) => {
+  // Note: using explicit types like `Auth.GoogleAuth` are only here for
+  // demonstration purposes.  Generally with TypeScript, these types would
+  // be inferred.
+  // const auth = new google.auth.GoogleAuth();
+  const calendar: calendar_v3.Calendar = google.calendar({
+    version: "v3",
+    auth: oauth2Client,
+  });
+
+  // // There are generated types for every set of request parameters
+  // const listParams: drive_v3.Params$Resource$Files$List = {};
+  // const res = await drive.files.list(listParams);
+
+  // // There are generated types for the response fields as well
+  // const listResults: drive_v3.Schema$FileList = res.data;
+  // const params: calendar_v3.Paramsj
+  // const auth: OAuth2Client = new google.auth.OAuth2(...);
+  // const calendar: Calendar = google.calendar({ version: 'v3', auth });
+  // const schemaEvent: Schema$Event = (await calendar.events.get({ calendarId, eventId })).data;
+  const events = await calendar.events.list({
+    calendarId: "primary",
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: "startTime",
+  });
+
+  response.json(events);
 });
 
 // app.get("/redirect", (request: Request, response: Response) => {
